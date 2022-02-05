50876c4250ae378d278ce8565e410a453ad32d08
diegoinjp <57879887+diegoinjp@users.noreply.github.com>
2022-02-05 23:46:13 +0900

    do base code, hero and nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import "./scss/styles.scss";
+import "../scss/styles.scss";
 
 //////////////////// DARK THEME
 
@@ -70,3 +70,23 @@ function linkAction() {
 }
 
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* Scroll
+
+
+
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+// const sr = ScrollReveal({
+//   origin: 'top',
+//   distance: '60px',
+//   duration: 2500,
+//   delay: 400,
+//   // reset: true
+// })
